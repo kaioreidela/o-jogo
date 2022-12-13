d3880d94d83feaff43a468f684014450ed15f792
kaioreidela <112896928+kaioreidela@users.noreply.github.com>
2022-12-13 12:25:58 -0300

    Update pyteste.py

--- pyteste.py
+++ pyteste.py
@@ -2,6 +2,7 @@ import pytest
 import pygame
 import variaveis
 
+pontos = 0
 
 def exibe_mensagem(msg, tamanho, cor):
     fonte = pygame.font.SysFont('comincsansms', tamanho, True, False)
@@ -11,25 +12,25 @@ def exibe_mensagem(msg, tamanho, cor):
 
 
 def test_exibe_mensagem_pontos():
-    textos_pontos = exibe_mensagem(variaveis.pontos, 40, (50, 10, 25))
-    assert textos_pontos == textos_pontos
+    textos_pontos = exibe_mensagem(pontos, 40, (50, 10, 25))
+    assert exibe_mensagem(pontos, 40, (50, 10, 25))  ==  textos_pontos
 
 
 def test_exibe_mensagem_restart():
     restart = variaveis.exibe_mensagem('press f to restart', 50, variaveis.Branco)
-    assert restart == restart
+    assert variaveis.exibe_mensagem('press f to restart', 50, variaveis.Branco) == restart
 
 def test_exibe_mensagem_tutorial():
     textos_tutorial= variaveis.exibe_mensagem("para pular aperte a tecla 'SPACE' ", 40, (50, 10, 25))
-    assert textos_tutorial == textos_tutorial
+    assert variaveis.exibe_mensagem("para pular aperte a tecla 'SPACE' ", 40, (50, 10, 25)) == textos_tutorial
 
 def test_exibe_mensagem_creditos():
     textos_creditos= variaveis.exibe_mensagem("creditos para todos que ajudaram no projeto", 40, variaveis.Preto)
-    assert textos_creditos == textos_creditos
+    assert variaveis.exibe_mensagem("creditos para todos que ajudaram no projeto", 40, variaveis.Preto) == textos_creditos
 
 def test_exibe_mensagem_dialogos():
     textos_dialogos= variaveis.exibe_mensagem("olá vamos ajudar raimundo a enfrentar os obstaculos que aparece em sua vida", 40, variaveis.Preto)
-    assert textos_dialogos == textos_dialogos
+    assert variaveis.exibe_mensagem("olá vamos ajudar raimundo a enfrentar os obstaculos que aparece em sua vida", 40, variaveis.Preto) == textos_dialogos
 
 
 
@@ -44,3 +45,4 @@ def test_reiniciajogo():
        assert variaveis.urubu.rect.x == Largura
        assert variaveis.cacto.rect.x == Largura
        assert v.escolha_obstaculo == choice([0, 1])
+
